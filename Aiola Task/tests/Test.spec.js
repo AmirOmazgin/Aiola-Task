13634a698c422f47b4a6ee05ee1b2b17d55b47c2
AmirOmazgin <81363766+AmirOmazgin@users.noreply.github.com>
2024-07-08 20:42:05 +0300

    Update Test.spec.js

--- Aiola Task/tests/Test.spec.js
+++ Aiola Task/tests/Test.spec.js
@@ -6,6 +6,6 @@ test('Launch movie', async ({ page }) => {
   await page.goto('https://www.nationalgeographic.com/');
   await expect(page.getByRole('heading', { name: 'Latest Stories' }).nth(1)).toBeVisible({ timeout: 10000 });
   const movieName = 'the-rise-and-fall-of-the-maya'
-  const hrefContains = `https://www.nationalgeographic.com/tv/shows/${movieName}`; // Adjust this to match your specific href criteria
+  const hrefContains = `https://www.nationalgeographic.com/tv/shows/${movieName}`; 
   await clickActiveSlideWithHref(page, hrefContains);
 });
